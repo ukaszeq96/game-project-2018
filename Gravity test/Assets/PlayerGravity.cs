ff2ecab2cb8fb5e9a85de32d5f2cde833315bd91
Lukasz Tomczyk <ukaszeq96@gmail.com>
2018-01-15 14:51:26 +0200

    initial gravity testing

---
+++ Gravity test/Assets/PlayerGravity.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGravity : MonoBehaviour {
+    //[SerializeField]
+    //Transform planet;
+
+    //[SerializeField]
+    //float gravitationpull;
+
+    [SerializeField]
+    float movementMultiplier;
+
+    private Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        rb.AddForce(new Vector2(Input.GetAxis("Horizontal") * movementMultiplier, Input.GetAxis("Vertical") * movementMultiplier));
+        //Vector2 diff = planet.transform.position - this.transform.position;
+        //rb.AddForce((diff).normalized * gravitationpull);
+        //float angle = Mathf.Atan2(diff.x, diff.y);
+        //this.transform.rotation = Quaternion.AngleAxis(angle, transform.up);
+	}
+}
