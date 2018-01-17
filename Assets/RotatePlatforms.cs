4f6410ea4b894abc5297804a1e0367a20ebf54ff
Lukasz Tomczyk <ukaszeq96@gmail.com>
2018-01-17 12:02:39 +0200

    added script for rotation of all the platforms

---
+++ Assets/RotatePlatforms.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePlatforms : MonoBehaviour
+{
+    [SerializeField]
+    Transform planet;
+
+    //private List<int> speeds = new List<int>();
+    // Use this for initialization
+    void Start()
+    {
+        //foreach (Transform child in transform)
+        //{
+        //    speeds.Add(Random.Range(0, 20));
+        //}
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //int i = 0;
+        foreach (Transform child in transform)
+        {
+            child.RotateAround(planet.position, Vector3.forward, 40 * Time.deltaTime);
+            //child.RotateAround(planet.position, Vector3.forward, speeds[i] * Time.deltaTime);
+            //i++;
+        }
+    }
+}
