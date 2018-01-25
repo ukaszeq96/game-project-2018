1fd0f38d82f1e213f93559c750d3e5ad3b2f424c
Bbikrampangeni <33446541+Bbikrampangeni@users.noreply.github.com>
2018-01-25 10:07:31 +0200

    camera follow
    chnaged the smooth camer a

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,10 +5,14 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform target;
     public float smoothSpeed = 0.125f;
+    public Vector3 offset;
 
-    void LateUpdate()
+    void FixedUpdate()
     {
-        transform.position = target.position;
+        Vector3 desiredposition = target.position + offset;
+        Vector3 smoothedpositon = Vector3.Lerp(transform.position,desiredposition,smoothSpeed * Time.deltaTime);
+        transform.position = smoothedpositon;
+        transform.LookAt(target);
     }
    
 }
--- Assets/Scripts/RotatePlatforms.cs
+++ Assets/Scripts/RotatePlatforms.cs
@@ -23,6 +23,7 @@ public class RotatePlatforms : MonoBehaviour
         foreach (Transform child in transform)
         {
             child.RotateAround(planet.position, Vector3.forward, 10 * Time.fixedDeltaTime);
+            print(transform.childCount);
             //child.RotateAround(planet.position, Vector3.forward, speeds[i] * Time.deltaTime);
             //i++;
             //   transform.position= new Vector3(Mathf.PingPong(Time.time, 3), transform.position.y, 0 );
