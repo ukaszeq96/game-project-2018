29a79c67951a25df5cff8163b4a492d0de934b7c
Lukasz Tomczyk <ukaszeq96@gmail.com>
2018-02-15 09:10:37 +0200

    Game Over screen now shows "Victory", when all parts were collected

--- Assets/Scripts/DisplayScore.cs
+++ Assets/Scripts/DisplayScore.cs
@@ -1,16 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DisplayScore : MonoBehaviour {
     private static int shipPartCount;
     public GameObject levelTime;
+    public Text gameOverText;
 	// Use this for initialization
 	void Start () {
         shipPartCount = PlayerController.shipPartCount;
         if(shipPartCount == 3)
         {
             levelTime.SetActive(true);
+            gameOverText.text = "Victory!";
         }
         else
         {
