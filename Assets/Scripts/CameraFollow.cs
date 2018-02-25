963d2d5b69a394998c7355d9307a712c6e8547fd
Miika Oja-Nisula <miika@oja-nisula.fi>
2018-02-25 17:55:39 +0200

    Make the camera follow the player with an offset (=> see less below the player, see more above)

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,10 +6,12 @@ public class CameraFollow : MonoBehaviour
     public Transform target;
     public float smoothRotation;
     public float smoothMovement;
+    
+    private float yOffset = 4.5f;
 
     void LateUpdate()
     {
-        transform.position = Vector3.Lerp(transform.position, target.transform.position, Time.deltaTime * smoothMovement);
+        transform.position = Vector3.Lerp(transform.position, target.transform.position + (target.transform.up * yOffset), Time.deltaTime * smoothMovement);
         transform.rotation = Quaternion.Euler(0, 0, Mathf.MoveTowardsAngle(transform.rotation.eulerAngles.z,target.transform.rotation.eulerAngles.z,Time.deltaTime * smoothRotation));
     }
 
