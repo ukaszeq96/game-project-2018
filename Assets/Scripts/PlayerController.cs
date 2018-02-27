3137bf61990355834483e568f1b9ccd2ffe19510
Miika Oja-Nisula <miika@oja-nisula.fi>
2018-02-27 19:29:03 +0200

    Refactor player controller, fix jump animation playing more than once per jump / ascent

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,6 +12,7 @@ public class PlayerController : MonoBehaviour
     public float movementSpeedAir;
     public float jumpSpeed;
     public int jetpackMax;
+    public float maxFalloutAngle;
     public Slider jetpackSlider;
 
     private Rigidbody2D rb;
@@ -26,10 +27,6 @@ public class PlayerController : MonoBehaviour
     private int addToShipPartCount;
     private bool isGrounded;
     
-    private float maxFalloutAngle = 2.5f;
-
-    private int frameCount = 0;
-    
     void Start()
     {
         shipPartCount = 0;
@@ -41,41 +38,22 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        frameCount += 1;
         verInput = Input.GetAxis("Jump");
         horInput = Input.GetAxis("Horizontal");
-        gravityDirection = transform.position - planet.position;
+
+        if (raycastHit.collider != null)
+            gravityDirection = raycastHit.normal;
+        else
+            gravityDirection = transform.position - planet.position;
         gravityDirection.Normalize();
         
-        if (raycastHit.collider != null) // all objects except PlatformTop are in IgnoreRaycast, so only when ray hits a platform raycasthit.collider != null
-        {
-            if (isGrounded)
-            {
-                // when on the ground, set (fix) the player transform rotation to prevent player from falling over
-                // rotation is fixed only when falling at an angle higher than maxFalloutAngle
-                if (Vector2.Angle(raycastHit.normal, transform.up) > maxFalloutAngle)
-                {
-                    transform.up = raycastHit.normal;
-                }
-            }
-            else
-                transform.up = Vector2.Lerp(transform.up, raycastHit.normal, Time.deltaTime * 3); // when in the air, rotate the player smoothly
-
-        }
-        else // when raycasthit.collider == null player rotates with respect to the surface of the planet
-        {
-            if (isGrounded)
-            {
-                // when on the ground, set (fix) the player transform rotation to prevent player from falling over
-                // rotation is fixed only when falling at an angle higher than maxFalloutAngle
-                if (Vector2.Angle(gravityDirection, transform.up) > maxFalloutAngle)
-                {
-                    transform.up = gravityDirection;
-                }
-            }
-            else
-                transform.up = Vector2.Lerp(transform.up, gravityDirection, Time.deltaTime * 3); // when in the air, rotate the player smoothly
-        }
+        // when on the ground, set (fix) the player transform rotation to prevent player from falling over
+        // rotation is fixed only when falling at an angle higher than maxFalloutAngle
+        if (Vector2.Angle(gravityDirection, transform.up) > maxFalloutAngle)
+            transform.up = gravityDirection;
+        else
+            transform.up = Vector2.Lerp(transform.up, gravityDirection, Time.deltaTime * 3); // when in the air, rotate the player smoothly
+        
         ver = transform.up;
         hor = Quaternion.Euler(0f, 0f, 90) * -ver;
         jetpackSlider.value = jetpack;
@@ -84,15 +62,6 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         raycastHit = Physics2D.Raycast(transform.position, -transform.up, 5); // cast a ray downwards from the player and return the collider hit by the vector as well as normal of the surface that was hit
-//        if (frameCount % 15 == 0)
-//        {
-//            bool isNull = (raycastHit.collider == null);
-//            print("Raycast collider null? " + isNull);
-//            if (raycastHit.collider == null)
-//            {
-////                print("Raycast hit: " + raycastHit);
-//            }
-//        }
         if (isGrounded)
         {
             rb.velocity = horInput * hor * movementSpeed;
@@ -101,33 +70,30 @@ public class PlayerController : MonoBehaviour
         }
         else
         {
-//            animator.SetTrigger("jump");
             rb.velocity = horInput * hor * movementSpeedAir;
         }
+        
         if (verInput > 0 && jetpack > 0)
-        {    
+        {
                 rb.AddForce(verInput * ver * jumpSpeed);
                 jetpack--;
         }
 
-        if (horInput < 0)
-        {
-            transform.localScale = new Vector3(transform.localScale.x < 0 ? transform.localScale.x : -1 * transform.localScale.x, transform.localScale.y, transform.localScale.z);
-            if (isGrounded)
-            {
-                animator.SetTrigger("run");
-            }
+        bool isJumping = animator.GetCurrentAnimatorStateInfo(0).IsName("jump");
+        bool isInTransition = animator.IsInTransition(0);
+        if (isGrounded && verInput > 0 && !isJumping && !isInTransition)
+            animator.SetTrigger("jump");
 
-        }
+        if (horInput < 0)
+            transform.localScale = new Vector3(transform.localScale.x < 0 ? transform.localScale.x : -1 * transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        
         else if (horInput > 0)
-        {
             transform.localScale = new Vector3(transform.localScale.x > 0 ? transform.localScale.x : -1 * transform.localScale.x, transform.localScale.y, transform.localScale.z);
-            if (isGrounded)
-            {
-                animator.SetTrigger("run");
-            }
-        }
-        else animator.SetTrigger("idle");
+
+        if (horInput != 0 && isGrounded)
+            animator.SetTrigger("run");
+        else
+            animator.SetTrigger("idle");
     }
 
     void LateUpdate()
@@ -144,7 +110,7 @@ public class PlayerController : MonoBehaviour
             if (other.gameObject.CompareTag("Platform"))
             {
                 platform = other.gameObject.GetComponent<Transform>();
-                this.transform.parent = platform.parent.transform;
+                transform.parent = platform.parent.transform;
             }
             isGrounded = true;
         }
@@ -161,10 +127,7 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("Platform") || other.gameObject.CompareTag("Planet"))
         {
             if (other.gameObject.CompareTag("Platform"))
-            {
-                this.transform.parent = null;
-
-            }
+                transform.parent = null;
             isGrounded = false;
         }
     }
