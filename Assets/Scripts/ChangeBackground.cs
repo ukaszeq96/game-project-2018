0b6f100e83df06fdb57012b94a3836be412c0525
Miika Oja-Nisula <miika@oja-nisula.fi>
2018-03-01 11:40:57 +0200

    Add background with a script to loop through different background (= stars not twinkling / stars twinkling)

---
+++ Assets/Scripts/ChangeBackground.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class ChangeBackground : MonoBehaviour
+{
+    public Sprite[] sprites;
+    public float[] intervals;
+
+    private int index;
+    private float lastUpdate;
+
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = sprites[index];
+    }
+
+    void Update()
+    {
+        float currentTime = Time.fixedUnscaledTime;
+        float interval = intervals[index];
+        if (currentTime >= lastUpdate + interval)
+        {
+            if (sprites.Length == index + 1)
+                index = 0;
+            else
+                index += 1;
+
+            GetComponent<SpriteRenderer>().sprite = sprites[index];
+            lastUpdate = currentTime;
+        }
+    }
+}
