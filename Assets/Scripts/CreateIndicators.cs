17f54d386493f6c30144c89ab6ef458e71e41f96
Lukasz Tomczyk <ukaszeq96@gmail.com>
2018-03-03 22:33:12 +0200

    Added arrow indicators for the ship parts and for the ship.

---
+++ Assets/Scripts/CreateIndicators.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateIndicators : MonoBehaviour {
+    GameObject[] parts;
+    public GameObject indicator;
+	// Update is called once per frame
+	void Start () {
+        parts = GameObject.FindGameObjectsWithTag("ShipPart");
+        foreach(GameObject part in parts)
+        {
+            GameObject partIndicator = Instantiate(indicator);
+            partIndicator.GetComponent<PointToObject>().objToFollow = part;
+            partIndicator.transform.parent = Camera.main.transform;
+        }
+	}
+}
