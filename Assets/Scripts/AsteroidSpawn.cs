a0400fcf9749e14496b7dfe1b7f94fe7c96d25b6
Lukasz Tomczyk <ukaszeq96@gmail.com>
2018-03-06 15:12:23 +0200

    Asteroids now spawn on the perimeter of a circle with a chosen radius.

--- Assets/Scripts/AsteroidSpawn.cs
+++ Assets/Scripts/AsteroidSpawn.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class AsteroidSpawn : MonoBehaviour {
     
     public GameObject asteroidobj; //exposed variable to pass the object
-    
+    public int asteroidMaxCount;
+    public float radius;
 	void Update () {
-	    if (Random.Range(0, 800) < 10) //every 10 in 1100 an asteroid is going to be generated
-	    {
-	        Vector3 pos = new Vector3(transform.position.x + Random.Range(-30f, 30f),
-	            transform.position.y + Random.Range(-30f, 30f),
-	            transform.position.z);
-	        if (GameObject.FindGameObjectsWithTag("Asteroid").Length < 20)
-	            Instantiate(asteroidobj, pos, transform.rotation);
-	    }
+       if(GameObject.FindGameObjectsWithTag("Asteroid").Length < asteroidMaxCount && Random.Range(0,100)<5)
+        {
+            Vector3 pos = RandomCircle(transform.position, radius); // asteroids are spawned at a random angle on the perimeter of the circle with a given radius and center at (0,0,1)
+            Vector3 vecToTarget = pos - transform.position;
+            float ang = Mathf.Atan2(vecToTarget.y, vecToTarget.x) * Mathf.Rad2Deg;
+            Quaternion rotation = Quaternion.AngleAxis(ang + 90, Vector3.forward);
+            Instantiate(asteroidobj,pos,rotation);
+        }
 	}
+    Vector3 RandomCircle(Vector3 center, float radius)
+    {
+        float angle = Random.value * 360;
+        Vector3 pos;
+        pos.x = center.x + radius * Mathf.Sin(angle * Mathf.Deg2Rad);
+        pos.y = center.y + radius * Mathf.Cos(angle * Mathf.Deg2Rad);
+        pos.z = transform.position.z;
+        return pos;
+    }
 }
