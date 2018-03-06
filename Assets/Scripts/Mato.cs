6ec1ad327cc7762784d80321fe1468e34067e175
Gisu-PC\Gisu <mirva.tapola@gmail.com>
2018-03-07 00:00:15 +0200

    Trying to solve worm issue !!!Do not use mato script!!!
    Mato script is causing my unity to crash. :D
    I'm too tired to understand anything. But here it is, fix it. :D

---
+++ Assets/Scripts/Mato.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mato : MonoBehaviour
+{
+    
+    protected Animator anim;
+    private GameObject mato;
+    public float speed = 1f;
+    public float pos = 0f;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        mato = GameObject.FindWithTag("Worm");
+        
+
+    }
+
+    void Update()
+    {
+        while(pos < 3.57f){
+        mato.transform.Translate(0, speed * Time.deltaTime, 0);
+    }
+
+
+    }
+
+    void FixedUpdate()
+    {
+        new WaitForSeconds(Random.Range(2, 9));
+        anim.SetTrigger(name: "appear");
+        new WaitForSeconds(Random.Range(3, 6));
+        anim.SetTrigger(name: "disappear");
+    }
+
+}
