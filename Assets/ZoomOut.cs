b2c307b326598c5511b12b7023d4ef48a8a9e947
Lukasz Tomczyk <ukaszeq96@gmail.com>
2018-03-07 21:06:04 +0200

    Added possibility to zoom out the camera while holding shift

---
+++ Assets/ZoomOut.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomOut : MonoBehaviour {
+    public float normalSize;
+    public float zoomedSize;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+
+            Camera.main.orthographicSize = zoomedSize;
+        else
+            Camera.main.orthographicSize = normalSize;
+	}
+}
